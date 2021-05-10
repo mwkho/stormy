4a538882c9fd486b9ff6530a6df67542f30c4d2e
Martin Ho <martin.h@hotmail.ca>
2021-05-10 12:08:35 -0700

    Remove apikey in axios call

--- app/lib/getHourlyWeather.js
+++ app/lib/getHourlyWeather.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 // getting hourly data to put into a graph/table
 
 const getHourlyWeather = (coordinates) => {
-  return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=94c45c13d9200c5e84dab55fa9169755&exclude=current,minutely,daily,alerts&units=metric`)
+  return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${ENV.OPENWEATHER_API_KEY}&exclude=current,minutely,daily,alerts&units=metric`)
   .then(res => {
     const hourly = res.data.hourly
     return 
