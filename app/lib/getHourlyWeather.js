fbe61f30306d4141e74cc2ea097c0e08d9750282
Martin Ho <martin.h@hotmail.ca>
2021-05-10 12:01:43 -0700

    Add weather icon link

--- app/lib/getHourlyWeather.js
+++ app/lib/getHourlyWeather.js
@@ -1,8 +1,24 @@
 const axios = require('axios');
 
 // getting hourly data to put into a graph/table
-axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${bt[0]}&lon=${bt[1]}&appid=94c45c13d9200c5e84dab55fa9169755&exclude=current,minutely,daily,alerts&units=metric`)
-.then(res => {
-  const hourly = res.data.hourly
-  return hourly
-})
+
+const getHourlyWeather = (coordinates) => {
+  return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=94c45c13d9200c5e84dab55fa9169755&exclude=current,minutely,daily,alerts&units=metric`)
+  .then(res => {
+    const hourly = res.data.hourly
+    return 
+  })
+}
+//waether icons can be found here
+// http://openweathermap.org/img/wn/10d@2x.png
+export default getHourlyWeather;
+
+// getting elavation of item
+// item
+//   .then((item) => {
+//     console.log(item[0].lat)
+//     return axios.get(`http://geogratis.gc.ca/services/elevation/cdem/altitude?lat=${item[0].lat}&lon=${item[0].lon}`)
+//     })
+//   .then((res) => {
+//     console.log(res.data.altitude)
+//   })
