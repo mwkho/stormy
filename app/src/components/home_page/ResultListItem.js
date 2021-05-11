a2b7a97e095bc07f731451ccf9b05c26c6715466
Martin Ho <martin.h@hotmail.ca>
2021-05-11 00:20:45 -0700

    Add an onclickhandler for each search query

--- app/src/components/home_page/ResultListItem.js
+++ app/src/components/home_page/ResultListItem.js
@@ -1,8 +1,13 @@
+import Axios from 'axios'
 import React from 'react'
 
 const ResultListItem  = (props) => {
+  const getInformation = (coordinates) => {
+  Axios.post("/information", {coordinates: coordinates})
+  }
+  
   return (
-    <li className="ResultListItem">
+    <li onClick={()=> getInformation(props.coordinates)} className="ResultListItem">
       <h2>{props.name}</h2>
     </li>
   )
