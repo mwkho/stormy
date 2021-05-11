0f9582a76a7d2f26779dfda0ea824a9767ab5d70
Martin Ho <martin.h@hotmail.ca>
2021-05-11 01:05:05 -0700

    Addigin display mode change and refactor

---
+++ app/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+// import "bootstrap/dist/bootstrap.min.css";
+import './App.css';
+import HomePage from './home_page/HomePage';
+import Favourites from './favourites/Favourites'
+
+export default function App(props) {
+
+  // different viewing modes for the web app
+  const HOME = 'HOME'
+  const INFORMATION = 'INFORMATION'
+  const FAVOURITES = 'FAVOURITES'
+  const [ page, setPage] = useState('HOME')
+
+    return (
+      <div className="App">
+        {page === HOME && <HomePage display={setPage}/>}
+        {page === INFORMATION && "Information page here"}
+        {page === FAVOURITES && <Favourites /> }
+      </div>
+    );
+}
