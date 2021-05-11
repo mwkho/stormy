6b826030afacd11e1d394929b8679d81d945236d
Martin Ho <martin.h@hotmail.ca>
2021-05-10 23:56:52 -0700

    Add retun statement for weather api call

--- router/helper/getHourlyWeather.js
+++ router/helper/getHourlyWeather.js
@@ -7,7 +7,7 @@ const OPENWEATHER_KEY = process.env.OPENWEATHER_API_KEY
   return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${OPENWEATHER_KEY}&exclude=current,minutely,daily,alerts&units=metric`)
   .then(res => {
     const hourly = res.data.hourly
-    return 
+    return hourly
   })
 }
 module.exports = getHourlyWeather;
