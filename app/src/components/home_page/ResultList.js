7a9bdf6a0bb82b31c821a98ec6d14e1d82d4077c
Martin Ho <martin.h@hotmail.ca>
2021-05-12 14:19:16 -0700

    Add map show on mouse hover, double mouse click to select

--- app/src/components/home_page/ResultList.js
+++ app/src/components/home_page/ResultList.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ResultListItem from './ResultListItem';
 
+
+
 const  ResultsList = (props) => {
+
   const resultList = props.results.map((result, index) => {
-    return <ResultListItem key={index} poi={result} setInformation={props.setInformation} display={props.display} setPOI={props.setPOI}/>
+    return <ResultListItem  key={index} poi={result} setInformation={props.setInformation} display={props.display} setPOI={props.setPOI}/>
   });
   return (
     <ul>
