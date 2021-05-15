85f9f44f64ff5939f45cb19edc46c6d0ea815876
Martin Ho <martin.h@hotmail.ca>
2021-05-15 09:23:11 -0700

    Making the avalanche problem pages

---
+++ app/src/components/information/AvalancheProblems.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+
+const AvalancheProblemsItem = (props) => {
+
+return(
+  
+    <Paper>
+
+    <TableContainer></TableContainer>
+    <Table></Table>
+    </Paper>
+      )
+}
+
+export default AvalancheProblemsItem;
--- app/src/components/information/AvalancheProblemsItem.js
+++ app/src/components/information/AvalancheProblemsItem.js
@@ -1,40 +1,49 @@
 import React from 'react';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+
 
 const AvalancheProblemsItem = (props) => {
   return (
     <>
-      <tr>
-        <th>{props.type}</th>
-      </tr>
-      <tr>
-        <td>{props.comment}</td>
-      </tr>
-      <tr>
-        <th>Elevation</th>
-      </tr>
-      <tr>
-        <td> {props.elevation} </td>
-      </tr>
-      <tr>
-        <th>Aspects</th>
-        <td></td>
-      </tr>
-      <tr>
-        <td>{props.aspects}</td>
-      </tr>
-      <tr>
-        <th>Expected size</th>
-        <th>Likelihood</th>
-      </tr>
-      <tr>
-        <td>{props.expectedSize}</td>
-        <td>{props.likelihood}</td>
-      </tr>
-      <tr>
-        <th>Aspects</th>
-        <td>{props.aspects}</td>
-      </tr>
+ 
+      <TableRow>
+        <TableCell varient="head">{props.type}</TableCell >
+      </TableRow>
+      <TableRow>
+        <TableCell>{props.comment}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell varient="head">Elevation</TableCell >
+      </TableRow>
+      <TableRow>
+        <TableCell> {props.elevation} </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell varient="head">Aspects</TableCell >
+        <TableCell></TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>{props.aspects}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell varient="head">Expected size</TableCell >
+        <TableCell varient="head">Likelihood</TableCell >
+      </TableRow>
+      <TableRow>
+        <TableCell>{props.expectedSize}</TableCell>
+        <TableCell>{props.likelihood}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell varient="head">Aspects</TableCell>
+        <TableCell>{props.aspects}</TableCell>
+      </TableRow>
     </>
+
   )
 }
 
