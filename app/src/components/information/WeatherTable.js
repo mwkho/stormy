34a60c83c89a0005c42d6dcc9b77ac1f1af64b30
Martin Ho <martin.h@hotmail.ca>
2021-05-17 02:22:21 -0700

    Formated weather table to have sticky headers

--- app/src/components/information/WeatherTable.js
+++ app/src/components/information/WeatherTable.js
@@ -7,80 +7,97 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+  stickyHeader: {
+    position:'sticky',
+    left:'0',
+    color: 'white',
+    background: 'grey'
+  },
+  caption:{
+    position:'static',
+  }
+})
 
 export default function WeatherTable(props){
-  const consolidate = props.weather
+  const consolidate = props.weather;
+  const classes = useStyles();
 
   const dateList = consolidate.dt.map((time, index) => {
-    const date = new Date(time*1000)
-    const hours = `${date.getHours()}:00`
-    return <TableCell key={"date-" + index}>{hours !== '0:00' ? hours : `${props.convertDate(date)}`+ "\n" +`${hours}`}</TableCell>
+    const date = new Date(time*1000);
+    const hours = `${date.getHours()}:00`;
+    const isZero = hours !== '0:00'
+    return <TableCell key={"date-" + index}>{ isZero ? hours : `${props.convertDate(date, isZero)}`}</TableCell>
   })
 
   return(
     <Paper>
       <TableContainer>
     <Table>
-    <caption> Weather data is provided by <a href="https://openweathermap.org/api/one-call-api">OpenWeatherMap</a>. </caption>
+      <caption className={classes.caption}> 
+        * total volume for the hour <br/>
+        Weather data is provided by <a href="https://openweathermap.org/api/one-call-api">OpenWeatherMap</a>. 
+      </caption>
       <TableHead>
         <TableRow>
-          <TableCell>Hour</TableCell>
+          <TableCell className={classes.stickyHeader}>Hour</TableCell>
           {dateList}
         </TableRow>
       </TableHead>
       <TableBody>
 
         <TableRow>
-          <TableCell></TableCell>
+          {/* <TableCell className={classes.stickyHeader}></TableCell> */}
+          <TableCell rowSpan={2} className={classes.stickyHeader} variant='head'>Weather</TableCell>
           {consolidate.icon.map((icon, index) => {
       return <TableCell key={"icon"+index}><img alt="" src={icon}/> </TableCell>
       })}
         </TableRow>
     <TableRow>
-    <TableCell variant='head'>Weather</TableCell>
     {consolidate.main.map((main, index) => {
       return <TableCell key={"main"+index}>{main}</TableCell>
     })}
   </TableRow>
     <TableRow>
-    <TableCell variant='head'>Weather Description</TableCell>
+    <TableCell className={classes.stickyHeader} variant='head'>Weather Description</TableCell>
     {consolidate.description.map((description, index) => {
       return <TableCell key={"description"+index}>{description}</TableCell>
     })}
   </TableRow>
   <TableRow>
-    <TableCell variant='head'>Temperature (°C)</TableCell>
+    <TableCell className={classes.stickyHeader} variant='head'>Temperature (°C)</TableCell>
     {consolidate.temp.map((temp, index) => {
       return <TableCell key={"temp"+index}>{temp}</TableCell>
     })}
   </TableRow>
   <TableRow>
-    <TableCell variant='head'>Wind Speed (m/s)</TableCell>
+    <TableCell className={classes.stickyHeader} variant='head'>Wind Speed (m/s)</TableCell>
     {consolidate.windSpeed.map((windSpeed, index) => {
       return <TableCell key={"windSpeed"+index}>{windSpeed}</TableCell>
     })}  
   </TableRow>
   <TableRow>
-    <TableCell variant='head'>Wind Degrees</TableCell>
+    <TableCell className={classes.stickyHeader} variant='head'>Wind Direction</TableCell>
     {consolidate.windDeg.map((windDeg, index) => {
       return <TableCell key={"windDeg"+index}>{windDeg}</TableCell>
     })}  
   </TableRow>
   <TableRow>
-    <TableCell variant='head'>Percent of Precipitation</TableCell>
+    <TableCell className={classes.stickyHeader} variant='head'>Percent of Precipitation</TableCell>
     {consolidate.pop.map((pop, index) => {
       return <TableCell key={"pop"+index}>{Math.round(pop*100) + "%"}</TableCell>
     })}  
   </TableRow>
   <TableRow>
-    <TableCell variant='head'>Rain <br/> (volume for last hour, mm)</TableCell>
+    <TableCell className={classes.stickyHeader} variant='head'>Rain (mm)*</TableCell>
     {consolidate.rain.map((rain, index) => {
       return <TableCell key={"rain"+index}>{rain ? rain : 0}</TableCell>
     })}  
   </TableRow>
   <TableRow>
-    <TableCell variant='head'>Snow <br/> (volume for last hour, mm)</TableCell>
+    <TableCell className={classes.stickyHeader} variant='head'>Snow (mm)*</TableCell>
     {consolidate.snow.map((snow, index) => {
       return <TableCell key={"snow"+index}>{snow ? snow : 0}</TableCell>
     })}  
