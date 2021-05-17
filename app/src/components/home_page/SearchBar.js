c63193f8fc128d2c753d1daacaacf27804d85fb6
Eli Kaplan <elikaplan@me.com>
2021-05-17 16:41:34 -0700

    added a universal background for the website

--- app/src/components/home_page/SearchBar.js
+++ app/src/components/home_page/SearchBar.js
@@ -27,7 +27,7 @@ export default function SearchBar(props){
         alignItems="center"
       >
         <TextField  
-          style={{ width: '80%', marginLeft: '165px' }}
+          style={{ width: '80%', marginRight: '100px', paddingLeft: '200px' }}
           multiline
           variant='filled'
           onChange={(event) => setValue(event.target.value)}
