fb748d57e5c4796c72d10be3c0eae867281a09f1
Eli Kaplan <elikaplan@me.com>
2021-05-17 18:02:14 -0700

    Refactor the search list to look a little cleaner

--- app/src/components/home_page/ResultListItem.js
+++ app/src/components/home_page/ResultListItem.js
@@ -1,6 +1,8 @@
 import Axios from 'axios'
 import React, { useState } from 'react'
 import MapItem from '../information/MapItem'
+import Button from '@material-ui/core/Button'
+import Tooltip from '@material-ui/core/Tooltip'
 
 const ResultListItem  = (props) => {
   const {poi, type} = props;
@@ -20,12 +22,27 @@ const ResultListItem  = (props) => {
   }
   
   return (
-    <li  onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} onDoubleClick={()=> selected(poi)} className="ResultListItem">
-      <h2>Name: {poi.name}</h2>
-      <h3>Region: {poi.region}</h3> 
-      {hover && <MapItem lat={poi.lat} lon={poi.lon}/>}
-    </li>
+    <>
+    <Button
+    onClick={()=> selected(poi)}
+    style={{backgroundColor: 'white', width: '800px'}}
+    onMouseEnter={() => setHover(true)}
+    onMouseLeave={() => setHover(false)}
+    >
+       {poi.name} <br></br>   Region: {poi.region}
+    </Button>
+    
+    {hover && <MapItem lat={poi.lat} lon={poi.lon}/>}
+    </>
+    
   )
 }
 
 export default ResultListItem;
+
+
+/*<li  onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} onDoubleClick={()=> selected(poi)} className="ResultListItem">
+      <h2>Name: {poi.name}</h2>
+      <h3>Region: {poi.region}</h3> 
+      {hover && <MapItem lat={poi.lat} lon={poi.lon}/>}
+    </li>*/
