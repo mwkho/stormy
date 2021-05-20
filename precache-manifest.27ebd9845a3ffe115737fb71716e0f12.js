dc11078cb7e7dd4468d8ff7cccf6494f481044ad
Martin Ho <martin.h@hotmail.ca>
2021-05-20 01:21:06 -0700

    Updates

---
+++ precache-manifest.27ebd9845a3ffe115737fb71716e0f12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e4691a13678b740a4a7",
+    "url": "/stormy/static/js/runtime~main.84ed0a15.js"
+  },
+  {
+    "revision": "9c87bcb209d4c4048070",
+    "url": "/stormy/static/js/main.c9474332.chunk.js"
+  },
+  {
+    "revision": "976cb296bcd71d12fdb0",
+    "url": "/stormy/static/js/2.20bff864.chunk.js"
+  },
+  {
+    "revision": "9c87bcb209d4c4048070",
+    "url": "/stormy/static/css/main.6eefedff.chunk.css"
+  },
+  {
+    "revision": "a0f3098506f1e89dd9a9b329a9ae53ad",
+    "url": "/stormy/index.html"
+  }
+];
