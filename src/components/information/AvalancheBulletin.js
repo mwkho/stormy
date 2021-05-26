a4aec047c368451cc35d6e7beb64ad8f8a6bae9f
Martin Ho <martin.h@hotmail.ca>
2021-05-26 16:14:01 -0700

    Remove example avalanche bulletin

--- src/components/information/AvalancheBulletin.js
+++ src/components/information/AvalancheBulletin.js
@@ -36,7 +36,7 @@ export default function AvalancheBulletin(props){
   const dangersList = dangerRatings.map((rating) => {
     const {alp, tln, btl} = rating.dangerRating
     const date = convertDate(new Date(rating.date))
-    return <AvalancheDangerItem date={date} alp={'1'} tln={'3'} btl={'4'}/> 
+    return <AvalancheDangerItem date={date} alp tln btl/> 
   });
 
   return(
