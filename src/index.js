210ca873b07e3e9630df14464852cf51b9468b13
Martin Ho <martin.h@hotmail.ca>
2021-05-26 10:16:09 -0700

    Add axios to app file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
